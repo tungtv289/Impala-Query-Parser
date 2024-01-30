a72960b1865bc3a92e3358aeeb0a3f32496df957
tungtv84 <tungtv84@ghtk.co>
2024-01-30 17:07:46 +0700

    added test case impala

--- src/test/java/ImpalaParserTest.java
+++ src/test/java/ImpalaParserTest.java
@@ -25,9 +25,18 @@ public class ImpalaParserTest {
 
     @Test
     public void select_with_join() throws IOException {
-        String stmt = Common.getStmtFromFile("impala_select_1.txt");
-        Set<TableStatic> actual = impalaParser.parser(stmt);
-        Assert.assertEquals(13, actual.size());
+        String impalaSelect1 = Common.getStmtFromFile("impala_select_1.txt");
+        Set<TableStatic> actual1 = impalaParser.parser(impalaSelect1);
+        String impalaSelect2 = Common.getStmtFromFile("impala_select_2.txt");
+        Set<TableStatic> actual2 = impalaParser.parser(impalaSelect2);
+
+        Assert.assertEquals(13, actual1.size());
+        Assert.assertEquals(7, actual2.size());
+
+        Assert.assertTrue(actual1.contains(new TableStatic(
+                TableStatic.CMD.SELECT_TABLE, "db_test", "stations")));
+        Assert.assertTrue(actual2.contains(new TableStatic(
+                TableStatic.CMD.SELECT_TABLE, "ghtk", "provinces_with_region_name")));
     }
 
     @Test
